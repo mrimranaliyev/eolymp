fb4ae21cc2a0c5dd1b3fca1a1f7bbd83e18db63d
mrimranaliyev <imranalyev237@gmail.com>
2024-10-11 18:20:05 +0400

    added commit

--- eolymp1/task/src/Main.java
+++ eolymp1/task/src/Main.java
@@ -1,17 +1,18 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        int[] arr = new int[n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = scanner.nextInt();
-        }
-        for (int i = 0; i < n; i++) {
-            System.out.print(arr[i] + " ");
-        }
 
+
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.
+        int[] array = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            array[i]= sc.nextInt();
+        }
+        for (int i = 0; i < n; i++) {
+            System.out.print(array[i]);
+
+        }
 
     }
 }
--- eolymp1/test8956/src/Main.java
+++ eolymp1/test8956/src/Main.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int [] array = new int[];
+        int  = i;
+
+        for (int = i;i < n ; i++) {
+            array [i] = sc.next;
+        }
 
 
 
