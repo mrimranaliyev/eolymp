d3d27a0c006680d55aedf56964e8f75c7d014ce0
mrimranaliyev <imranalyev237@gmail.com>
2024-10-14 18:03:31 +0400

    added commit

---
+++ task494/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+    String s = "abrakadabra";
+    int a = 3;
+    int b = 6;
+
+    String sNew = s.substring(0,a) + s.substring(b + 1);
+
+    System.out.println(sNew);
+
+
+    }
+}
--- test909/src/Main.java
+++ test909/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        String text = "hello world how are you ";
+        String text = "hello world how are you today ";
         String[] sözlər = text.trim().split("\\s+");
         int sözSayı = sözlər.length;
         System.out.println("Sözlərin sayı: " + sözSayı);
