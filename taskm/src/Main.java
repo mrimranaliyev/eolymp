63d3acc3fe4945fc4c9fdebbd945a759c8d13649
mrimranaliyev <imranalyev237@gmail.com>
2024-10-14 19:13:53 +0400

    added commit

---
+++ taskm/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+        char [] c = s.toCharArray();
+        c[0] = Character.toUpperCase(c[0]);
+        for (int i = 1; i < c.length - 1; i++) {
+            if (c[i] == ' ') {
+                c[i + 1] = Character.toUpperCase(c[i + 1]);
+            }
+            System.out.println(c);
+
+        }
+    }
+}
