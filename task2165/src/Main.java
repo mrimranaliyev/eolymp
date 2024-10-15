757463b13b620b37294ac631d1d807c0261651af
mrimranaliyev <imranalyev237@gmail.com>
2024-10-15 15:24:48 +0400

    added committ

---
+++ task2165/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+        s = s.trim().replaceAll("\\s+"," ");
+        System.out.println(s);
+
+
+
+
+    }
+}
--- test345/src/Main.java
+++ test345/src/Main.java
@@ -1,10 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        printLetters();
-    }
-    public static void printLetters() {
-        for (char c = 'a'; c <= 'z'; c++) {
-            System.out.print(c + " ");
-        }
+
     }
 }
