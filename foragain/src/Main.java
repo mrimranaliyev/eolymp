d36ab5f9bd3f988fcd70474bfa1df07cc6821195
mrimranaliyev <imranalyev237@gmail.com>
2024-10-20 10:28:47 +0400

    commit added

---
+++ foragain/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        boolean d = true;
+        int s = 0;
+        while (d == true) {
+            System.out.println("hello");
+            s++;
+            if (s == 10) {
+                d = false;
+
+            }
+        }
+    }
+}
